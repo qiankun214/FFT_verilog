05261f1e203c20d8d938c132b2ee234eb8d83271
qiankun <qiankun96214@outlook.com>
2019-07-05 17:21:09 +0800

    add input reshape

--- src/full_parallel_fft/butterfly.v
+++ src/full_parallel_fft/butterfly.v
@@ -29,6 +29,24 @@ module butterfly#(
 
 localparam GL = 2 ** STEP;
 
+// input reshape
+genvar input_i;
+wire [WIDTH - 1:0] this_data_real [2 ** NPOINT - 1:0];
+wire [WIDTH - 1:0] this_data_imag [2 ** NPOINT - 1:0];
+generate
+	if(STEP == 0) begin:is_first
+		for (input_i = 0; input_i < 2 ** NPOINT; input_i++) begin
+			// wire [NPOINT - 1:0]tmp = input_i;
+			assign this_data_real[input_i[0:NPOINT-1] * WIDTH +: WIDTH] = din_real[input_i * WIDTH +: WIDTH];
+			assign this_data_real[input_i[0:NPOINT-1] * WIDTH +: WIDTH] = din_imag[input_i * WIDTH +: WIDTH];
+		end
+	end else begin
+		assign this_data_real = din_real;
+		assign this_data_imag = din_imag;
+	end
+endgenerate
+
+
 // split
 genvar split_i;
 wire [WIDTH - 1:0] din_data_real [2 ** NPOINT - 1:0];
@@ -39,12 +57,12 @@ wire signed [WIDTH - 1:0] butterfly_op1_imag [2 ** (NPOINT - 1) - 1:0];
 wire signed [WIDTH - 1:0] butterfly_op2_real [2 ** (NPOINT - 1) - 1:0];
 wire signed [WIDTH - 1:0] butterfly_op2_imag [2 ** (NPOINT - 1) - 1:0];
 generate
-	for (int split_i = 0; split_i < 2 ** NPOINT; split_i++) begin:split_din
-		assign din_data_real[split_i] = din_real[split_i * WIDTH +: WIDTH];
-		assign din_data_imag[split_i] = din_imag[split_i * WIDTH +: WIDTH];
+	for (split_i = 0; split_i < 2 ** NPOINT; split_i++) begin:split_din
+		assign din_data_real[split_i] = this_data_real[split_i * WIDTH +: WIDTH];
+		assign din_data_imag[split_i] = this_data_imag[split_i * WIDTH +: WIDTH];
 	end
 
-	for (int split_i = 0; split_i < 2 ** (NPOINT - 1); split_i++) begin:con_din
+	for (split_i = 0; split_i < 2 ** (NPOINT - 1); split_i++) begin:con_din
 		assign butterfly_op1_real[split_i] = din_data_real[ (split_i >> STEP) * 2 * GL + split_i % GL ];
 		assign butterfly_op1_imag[split_i] = din_data_imag[ (split_i >> STEP) * 2 * GL + split_i % GL ];
 		assign butterfly_op2_real[split_i] = din_data_real[ (split_i >> STEP) * 2 * GL + split_i % GL + GL ];
@@ -60,7 +78,7 @@ wire signed [WIDTH - 1:0] butterfly_tmp_real [2 ** (NPOINT - 1) - 1:0];
 wire signed [WIDTH - 1:0] butterfly_tmp_imag [2 ** (NPOINT - 1) - 1:0];
 genvar compute_i;
 generate
-	for (int compute_i = 0; compute_i < 2 ** (NPOINT - 1); compute_i++) begin:compute
+	for (compute_i = 0; compute_i < 2 ** (NPOINT - 1); compute_i++) begin:compute
 		
 		wire signed [2 * WIDTH - 1:0] tmp11 = weight_real * butterfly_op2_real;
 		wire signed [2 * WIDTH - 1:0] tmp12 = weight_imag * butterfly_op2_imag;
@@ -86,14 +104,14 @@ wire [WIDTH - 1:0] dout_data_imag [2 ** NPOINT - 1:0];
 wire [WIDTH - 1:0] dout_real_com [2 ** NPOINT - 1:0];
 wire [WIDTH - 1:0] dout_imag_com [2 ** NPOINT - 1:0];
 generate
-	for (int allocation_i = 0; allocation_i < 2 ** (NPOINT - 1); allocation_i++) begin:allocation_dout
+	for (allocation_i = 0; allocation_i < 2 ** (NPOINT - 1); allocation_i++) begin:allocation_dout
 		assign dout_data_real[ (allocation_i >> STEP) * 2 * GL + allocation_i % GL ] 		= 	butterfly_r1_real[allocation_i];
 		assign dout_data_imag[ (allocation_i >> STEP) * 2 * GL + allocation_i % GL ] 		= 	butterfly_r1_imag[allocation_i];
 		assign dout_data_real[ (allocation_i >> STEP) * 2 * GL + allocation_i % GL + GL ] 	= 	butterfly_r2_real[allocation_i];
 		assign dout_data_imag[ (allocation_i >> STEP) * 2 * GL + allocation_i % GL + GL ] 	= 	butterfly_r2_imag[allocation_i];
 	end
 
-	for (int allocation_i = 0; allocation_i < 2 ** NPOINT; allocation_i++) begin:assign_dout
+	for (allocation_i = 0; allocation_i < 2 ** NPOINT; allocation_i++) begin:assign_dout
 		assign dout_real_com[allocation_i * WIDTH +: WIDTH] = dout_data_real[allocation_i];
 		assign dout_imag_com[allocation_i * WIDTH +: WIDTH] = dout_data_imag[allocation_i];
 	end
