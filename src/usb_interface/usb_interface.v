55030b007fac00a77dcf4ccba4fa27a039376d38
qiankun <qiankun96214@outlook.com>
2019-07-16 00:00:56 +0800

    work on usb interface

--- src/usb_interface/usb_interface.v
+++ src/usb_interface/usb_interface.v
@@ -33,9 +33,9 @@ module usb_interface #(parameter int NPOINT = 3)(
 );
 // usb
 wire is_din;
+wire is_dout;
 
-
-// inter fsm
+// din fsm
 localparam INIT = 2'b00;
 localparam WIGH = 2'b01;
 localparam DATA = 2'b11;
@@ -44,41 +44,48 @@ reg [NPOINT + NPOINT - 1:0] weight_count;
 always @ (posedge clk or negedge rst_n) begin
 	if(~rst_n) begin
 		weight_count <= 'b0;
-	end else if(inter_mode == WIGH && is_din) begin
+	end else if(din_mode == WIGH && is_din) begin
 		weight_count <= weight_count + 1'b1;
-	end else begin
-		weight_count <= 'b0;
 	end
 end
 
-reg [1:0] inter_mode,inter_next_mode;
+reg [NPOINT:0] data_count;
 always @ (posedge clk or negedge rst_n) begin
 	if(~rst_n) begin
-		inter_mode <= 'b0;
+		data_count <= 'b0;
+	end else if(din_mode == DATA && is_din) begin
+		data_count <= data_count + 1'b1;
+	end
+end
+
+reg [1:0] din_mode,next_din_mode;
+always @ (posedge clk or negedge rst_n) begin
+	if(~rst_n) begin
+		din_mode <= 'b0;
 	end else begin
-		inter_mode <= inter_next_mode;
+		din_mode <= next_din_mode;
 	end
 end
 
 always @ (*) begin
-	case (inter_mode)
-		INIT:inter_next_mode = WIGH;
+	case (din_mode)
+		INIT:next_din_mode = WIGH;
 		WIGH:begin
-			if(weight_count == 2 * NPOINT * (2 ** (NPOINT - 1))) begin
-				inter_next_mode = DATA;
+			if(weight_count == 2 * NPOINT * (2 ** (NPOINT - 1)) && is_din) begin
+				next_din_mode = DATA;
 			end else begin
-				inter_next_mode = WIGH;
+				next_din_mode = WIGH;
 			end
 		end
-		DATA:inter_next_mode = DATA;
-		default:inter_next_mode = INIT;
+		DATA:next_din_mode = DATA;
+		default:next_din_mode = INIT;
 	endcase
 end
 
 always @ (posedge clk or negedge rst_n) begin
 	if(~rst_n) begin
 		fft_weight_valid <= 'b0;
-	end else if(mode == WIGH && is_din) begin
+	end else if(din_mode == WIGH && is_din) begin
 		fft_weight_valid <= weight_count[0];
 	end else begin
 		fft_weight_valid <= 'b0;
@@ -88,7 +95,7 @@ end
 always @ (posedge clk or negedge rst_n) begin
 	if(~rst_n) begin
 		fft_weight_real <= 'b0;
-	end else if(mode == WIGH && is_din && !weight_count[0]) begin
+	end else if(din_mode == WIGH && is_din && !weight_count[0]) begin
 		fft_weight_real <= fx2_db;
 	end
 end
@@ -96,9 +103,76 @@ end
 always @ (posedge clk or negedge rst_n) begin
 	if(~rst_n) begin
 		fft_weight_imag <= 'b0;
-	end else if(mode == WIGH && is_din && weight_count[0]) begin
+	end else if(din_mode == WIGH && is_din && weight_count[0]) begin
 		fft_weight_imag <= fx2_db;
 	end
+end
+
+always @ (posedge clk or negedge rst_n) begin
+	if(~rst_n) begin
+		fft_din_real <= 'b0;
+	end else if(din_mode == DATA && is_din && !data_count[0]) begin
+		fft_din_real <= {fx2_db,fft_din_real[16 * (2 ** NPOINT) - 1:15]};
+	end
+end
+
+always @ (posedge clk or negedge rst_n) begin
+	if(~rst_n) begin
+		fft_din_imag <= 'b0;
+	end else if(din_mode == DATA && is_din && data_count[0]) begin
+		fft_din_imag <= {fx2_db,fft_din_imag[16 * (2 ** NPOINT) - 1:15]};
+	end
+end
+
+always @ (posedge clk or negedge rst_n) begin
+	if(~rst_n) begin
+		fft_din_valid <= 'b0;
+	end else if(din_mode == DATA && is_din && data_count[0] == 2 ** (NPOINT+1)-1) begin
+		fft_din_valid <= 1'b1;
+	end else begin
+		fft_din_valid <= 'b0;
+	end
+end
+
+// dout fsm
+localparam DOUT_INIT = 1'b0;
+localparam DOUT_WORK = 1'b1;
+reg [NPOINT:0] result_count;
+always @ (posedge clk or negedge rst_n) begin
+	if(~rst_n) begin
+		result_count <= 'b0;
+	end else if(dout_mode == DOUT_WORK && is_dout) begin
+		result_count <= result_count + 1'b1;
+	end
+end
+
+reg dout_mode,next_dout_mode;
+always @ (posedge clk or negedge rst_n) begin
+	if(~rst_n) begin
+		dout_mode <= DOUT_INIT;
+	end else begin
+		dout_mode <= next_din_mode;
+	end
+end
+
+always @ (*) begin
+	case (dout_mode)
+		DOUT_INIT:begin
+			if(fft_dout_valid) begin
+				next_dout_mode <= DOUT_WORK;
+			end else begin
+				next_dout_mode <= DOUT_INIT;
+			end
+		end
+		DOUT_WORK:begin
+			if(result_count == 2 ** (NPOINT+1)-1 && is_dout) begin
+				next_dout_mode <= DOUT_INIT;
+			end else begin
+				next_dout_mode <= DOUT_WORK;
+			end
+		end
+		default:next_dout_mode <= DOUT_INIT;
+	endcase
 end
 
 endmodule
